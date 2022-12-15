3caf0e0ac3f428b717b83ddfc9a0329db24350c4
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-12-15 10:13:05 -0300

    getters, botões pra mostrar todas ou favoritas

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,21 @@
       <h1>Tarefas</h1>
     </header>
 
-    <div class="lista-tarefas">
+    <nav class="filtro">
+      <button @click="filtro = 'tudo'">     Todas</button>
+      <button @click="filtro = 'favoritas'">Favoritas</button>
+    </nav>
+
+    <div class="lista-tarefas" v-if="filtro === 'tudo'">
+      <p>{{tarefaStore.total}} tarefas totais restantes</p>
       <div v-for="tarefa in tarefaStore.tarefas" :key="tarefa.id">
+        <DetalhesTarefa :tarefa="tarefa"/>
+      </div>
+    </div>
+
+    <div class="lista-tarefas" v-if="filtro === 'favoritas'">
+      <p>{{tarefaStore.qtFavoritas}} tarefas favoritas restantes</p>
+      <div v-for="tarefa in tarefaStore.favoritas" :key="tarefa.id">
         <DetalhesTarefa :tarefa="tarefa"/>
       </div>
     </div>
@@ -14,14 +27,16 @@
 </template>
 
 <script>
+  import {ref} from "vue";
   import DetalhesTarefa from "./components/DetalhesTarefa.vue";
   import {useTarefaStore} from "./store/TarefaStore";
 
   export default {
     components: {DetalhesTarefa},
     setup(){
+      const filtro = ref("tudo");
       const tarefaStore = useTarefaStore();
-      return {tarefaStore};
+      return {tarefaStore, filtro};
     }
   }
 </script>
--- src/store/TarefaStore.js
+++ src/store/TarefaStore.js
@@ -6,5 +6,18 @@ export const useTarefaStore = defineStore("tarefaStore", {
       {id: 1, titulo: "comprar pão", isFav: false},
       {id: 2, titulo: "jogar W&R",   isFav: true}
     ]
-  })
+  }),
+  getters: {
+    favoritas(){
+      return this.tarefas.filter(t => t.isFav);
+    },
+    qtFavoritas(){
+      return this.tarefas.reduce((ct, tarefa) => {
+        return tarefa.isFav ? ct + 1 : ct;
+      }, 0);
+    },
+    total: (state) => {
+      return state.tarefas.length
+    }
+  }
 });
