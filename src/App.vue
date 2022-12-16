ff8b7d94a3c3a5c06fe0c5539259f2582824f344
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-12-16 11:39:58 -0300

    storeToRefs

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,18 @@
       <button @click="filtro = 'favoritas'">Favoritas</button>
     </nav>
 
-    <div class="loading" v-if="tarefaStore.loading">Carregando tarefas...</div>
+    <div class="loading" v-if="loading">Carregando tarefas...</div>
 
     <div class="lista-tarefas" v-if="filtro === 'tudo'">
-      <p>{{tarefaStore.total}} tarefas totais restantes</p>
-      <div v-for="tarefa in tarefaStore.tarefas" :key="tarefa.id">
+      <p>{{total}} tarefas totais restantes</p>
+      <div v-for="tarefa in tarefas" :key="tarefa.id">
         <DetalhesTarefa :tarefa="tarefa"/>
       </div>
     </div>
 
     <div class="lista-tarefas" v-if="filtro === 'favoritas'">
-      <p>{{tarefaStore.qtFavoritas}} tarefas favoritas restantes</p>
-      <div v-for="tarefa in tarefaStore.favoritas" :key="tarefa.id">
+      <p>{{qtFavoritas}} tarefas favoritas restantes</p>
+      <div v-for="tarefa in favoritas" :key="tarefa.id">
         <DetalhesTarefa :tarefa="tarefa"/>
       </div>
     </div>
@@ -33,18 +33,22 @@
 </template>
 
 <script>
-  import {ref} from "vue";
-  import DetalhesTarefa from "./components/DetalhesTarefa.vue";
-  import FormTarefa from "./components/FormTarefa.vue";
-  import {useTarefaStore} from "./store/TarefaStore";
+import {ref} from "vue";
+import DetalhesTarefa from "./components/DetalhesTarefa.vue";
+import FormTarefa from "./components/FormTarefa.vue";
+import {useTarefaStore} from "./store/TarefaStore";
+import { storeToRefs } from "pinia";
 
-  export default {
-    components: {DetalhesTarefa, FormTarefa},
-    setup(){
-      const filtro = ref("tudo");
-      const tarefaStore = useTarefaStore();
-      tarefaStore.getTarefas();
-      return {tarefaStore, filtro};
-    }
+export default {
+  components: {DetalhesTarefa, FormTarefa},
+  setup(){
+    const filtro = ref("tudo");
+    const tarefaStore = useTarefaStore();
+
+    const {tarefas, loading, favoritas, total, qtFavoritas} = storeToRefs(tarefaStore);
+
+    tarefaStore.getTarefas();
+    return {tarefaStore, filtro, tarefas, loading, favoritas, total, qtFavoritas};
   }
+}
 </script>
